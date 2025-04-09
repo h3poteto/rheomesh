8dbeafba9a4cae104eb49281069f566a219c9e70
AkiraFukushima <h3.poteto@gmail.com>
2025-04-09 18:12:06 +0900

    refs #49 Update screen sharing e2e for multiple video elements

--- e2e/src/screen.spec.ts
+++ e2e/src/screen.spec.ts
@@ -103,6 +103,85 @@ test("Screen", async () => {
 
   expect(video2Time2).toBeGreaterThan(video2Time1);
 
+  // Add browser3
+  const browser3 = await chromium.launch({
+    args: [
+      "--use-fake-ui-for-media-stream",
+      "--use-fake-device-for-media-stream",
+    ],
+  });
+
+  const context3 = await browser3.newContext();
+  const page3 = await context3.newPage();
+  await page3.goto("http://localhost:3000/room?room=example");
+  await page3.click("#connect");
+  await page3.waitForFunction(() => {
+    const button = document.querySelector("#capture") as HTMLButtonElement;
+    return button && button.disabled === false;
+  });
+  // Confirm browser3 is subscribing the video
+  await page3.waitForFunction(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video && video.readyState > 2;
+  });
+  const videoState3 = await page3.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+
+  console.log(`Browser 3 video state: ${videoState3}`);
+  expect(videoState3).toBeGreaterThan(2);
+
+  // Start publishing video from browser3
+  await page3.click("#capture");
+  await page3.waitForSelector("#sending-video");
+  await page3.waitForFunction(() => {
+    const video = document.querySelector("#sending-video") as HTMLVideoElement;
+    return video && video.readyState > 2;
+  });
+  const videoState4 = await page3.evaluate(() => {
+    const video = document.querySelector("#sending-video") as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+  console.log(`Browser 3 video state: ${videoState4}`);
+  expect(videoState4).toBeGreaterThan(2);
+
+  // Browser1 can receive the video from browser3
+  await page1.waitForSelector(".receiving-video");
+  await page1.waitForFunction(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video && video.readyState > 2;
+  });
+  const videoState5 = await page1.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+  console.log(`Browser 1 video state: ${videoState5}`);
+  expect(videoState5).toBeGreaterThan(2);
+
+  // Browser2 can receive the video from browser3. It should have 2 video elements
+  await page2.waitForSelector(".receiving-video");
+  const videoLocator = page2.locator(".receiving-video");
+  const count = await videoLocator.count();
+  expect(count).toBe(2);
+
+  for (let i = 0; i < count; i++) {
+    const videoElement = videoLocator.nth(i);
+    const videoState6 = await videoElement.evaluate((video) => {
+      return (video as HTMLVideoElement).readyState;
+    });
+    console.log(`Browser 2 video state: ${videoState6}`);
+    expect(videoState6).toBeGreaterThan(2);
+  }
+
   await browser1.close();
   await browser2.close();
 });
