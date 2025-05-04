d6fc2773dfeaf5380565531fb7657b4961f87f00
AkiraFukushima <h3.poteto@gmail.com>
2025-05-04 21:46:23 +0900

    refs #49 Add E2E tests for Restart ICE

--- e2e/src/screen.spec.ts
+++ e2e/src/screen.spec.ts
@@ -187,3 +187,95 @@ test("Screen", async ({ browserType }) => {
   await browser1.close();
   await browser2.close();
 });
+
+test("Restart ICE", async ({ browserType }) => {
+  const browser1 = await browserType.launch({});
+  const browser2 = await browserType.launch({});
+
+  const context1 = await browser1.newContext();
+  const page1 = await context1.newPage();
+
+  const context2 = await browser2.newContext();
+  const page2 = await context2.newPage();
+
+  await page1.goto("http://localhost:3000/room?room=example-ice");
+  await page2.goto("http://localhost:3000/room?room=example-ice");
+
+  await page1.click("#connect");
+  await page2.click("#connect");
+
+  await page1.waitForFunction(() => {
+    const button = document.querySelector("#capture") as HTMLButtonElement;
+    return button && button.disabled === false;
+  });
+
+  await page1.click("#capture");
+
+  await page1.waitForSelector("#sending-video");
+
+  await page2.waitForSelector(".receiving-video");
+
+  await page2.waitForFunction(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video && video.readyState > 2;
+  });
+
+  const videoState1 = await page1.evaluate(() => {
+    const video = document.querySelector("#sending-video") as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+
+  const videoState2 = await page2.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+
+  expect(videoState1).toBeGreaterThan(2);
+  expect(videoState2).toBeGreaterThan(2);
+
+  const video2Time1 = await page2.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.currentTime : -1;
+  });
+
+  console.log(`Browser 2 video current time: ${video2Time1}`);
+
+  await page1.waitForTimeout(10000); // Wait for 10 seconds
+
+  const video2Time2 = await page2.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.currentTime : -1;
+  });
+
+  console.log(`Browser 2 video current time: ${video2Time2}`);
+  expect(video2Time2).toBeGreaterThan(video2Time1);
+
+  // Restart ICE
+  await page2.click("#restart");
+
+  await page2.waitForTimeout(5000); // Wait for 5 seconds
+
+  const videoState3 = await page2.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+  expect(videoState3).toBeGreaterThan(2);
+  const video2Time3 = await page2.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.currentTime : -1;
+  });
+  console.log(`Browser 2 video current time: ${video2Time3}`);
+  expect(video2Time3).toBeGreaterThan(video2Time2);
+});
