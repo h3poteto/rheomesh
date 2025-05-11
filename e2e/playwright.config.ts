17daef3965602f51b283a6f1f026bfd3a5442e62
AkiraFukushima <h3.poteto@gmail.com>
2025-05-11 16:26:10 +0900

    Update e2e test for relay

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -2,6 +2,7 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  timeout: 60 * 1000,
   workers: 1,
   use: {
     locale: "en-US",
--- e2e/src/relay.spec.ts
+++ e2e/src/relay.spec.ts
@@ -110,6 +110,54 @@ test("Relay", async ({ browserType }) => {
 
   expect(video2Time2).toBeGreaterThan(video2Time1);
 
+  // Reload browser 2 and check if the video is still playing
+  await context2.close();
+  const context3 = await browser2.newContext();
+  const page3 = await context3.newPage();
+  await page3.goto("http://localhost:3002/room?room=example-relay");
+  await page3.waitForSelector("#connect");
+  await page3.click("#connect");
+  await page3.waitForSelector(".receiving-video");
+  await page3.waitForFunction(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video && video.readyState > 2;
+  });
+  const video3State = await page3.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.readyState : -1;
+  });
+  console.log(`Browser 2 reloaded video state: ${video3State}`);
+
+  expect(video3State).toBeGreaterThan(2);
+  const video3Paused = await page3.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.paused : true;
+  });
+  console.log(`Browser 2 reloaded video paused: ${video3Paused}`);
+  expect(video3Paused).toBe(false);
+  const video3Time = await page3.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.currentTime : -1;
+  });
+  console.log(`Browser 2 reloaded video current time: ${video3Time}`);
+  await page3.waitForTimeout(10000); // Wait for 10 seconds
+  const video3Time2 = await page3.evaluate(() => {
+    const video = document.querySelector(
+      ".receiving-video",
+    ) as HTMLVideoElement;
+    return video ? video.currentTime : -1;
+  });
+  console.log(`Browser 2 reloaded video current time: ${video3Time2}`);
+  expect(video3Time2).toBeGreaterThan(video3Time);
+
   await browser1.close();
   await browser2.close();
 });
